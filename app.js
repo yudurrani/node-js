5d85f56706b90e2de66da39d0c8886ef23d87216
Yasir Durrani <yudurrani@gmail.com>
2019-05-12 03:09:45 -0400

    The Nodemon

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
+const color = require('chalk')
 const notes = require('./notes.js')
 
 const getNotes = notes()
 
-console.log(getNotes)
+console.log(color.red.inverse(getNotes))
+
+console.log(color.blue('i am blue'))
 
 
 
+// const validator = require('validator')
 
+// const notes = require('./notes.js')
+
+// const chalk = require('chalk')
+
+// const getNotes = notes()
+
+// console.log(getNotes)
+
+// console.log(validator.isEmail('yasir@gmails.ca'))
+
+// console.log(validator.isURL('www.gmail.com'))
+
+// console.log(chalk.blue('Hi there'))
+
+// console.log(chalk.red.bold('Hi there'))
 
 // const minus=require('./utils.js')
 
