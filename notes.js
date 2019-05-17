3546f20fc3eb8711a388137b19f7443119eea11f
Yasir Durrani <yudurrani@gmail.com>
2019-05-16 20:23:57 -0400

    updated notes.js

--- notes.js
+++ notes.js
@@ -1,32 +1,71 @@
+const fs = require('fs')
 
 const getNotes = function(){
-   return ' The Notes are as follows:'
+   return 'Your Notes...'
 }
 
 const addNote = function(title, body){
    const notes = loadNotes()
-   console.log(notes)
 
+   notes.push({
+      title : title,
+      body : body,
+
+   })
+
+   saveNotes(notes)
+}
+
+const saveNotes = function(notes){
+   const dataJSON = JSON.stringify(notes)
+   fs.writeFileSync('notees.json', dataJSON)
 }
 
 const loadNotes = function(){
-   try {
-      const databuffer = fs.readFileSync('notes.json')
-      const dataJSON = databuffer.toString()
-      return JSON.parse(dataJSON)
-
+   try{
+   const databuffer = fs.readFileSync('notees.json')
+   const dataJSON = databuffer.toString()
+   return JSON.parse(dataJSON)
    } catch(e){
-      return[]
-
+      return []
    }
-
-  
-
 }
 
 module.exports = {
-   getNotes: getNotes,
-   addNote: addNote,
-
+   getNotes : getNotes,
+   addNote : addNote
 }
+
+
+
+// const getNotes = function(){
+//    return ' The Notes are as follows:'
+// }
+
+// const addNote = function(title, body){
+//    const notes = loadNotes()
+//    console.log(notes)
+
+// }
+
+// const loadNotes = function(){
+//    try {
+//       const databuffer = fs.readFileSync('notes.json')
+//       const dataJSON = databuffer.toString()
+//       return JSON.parse(dataJSON)
+
+//    } catch(e){
+//       return[]
+
+//    }
+
+  
+
+// }
+
+// module.exports = {
+//    getNotes: getNotes,
+//    addNote: addNote,
+
+// }
 
