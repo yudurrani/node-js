11413dfceb6cd4f4aab64be3a03edfaa2842eb9b
Yasir Durrani <yudurrani@gmail.com>
2019-05-17 05:05:57 -0400

    adding notes completed

--- notes.js
+++ notes.js
@@ -6,14 +6,24 @@ const getNotes = function(){
 
 const addNote = function(title, body){
    const notes = loadNotes()
-
-   notes.push({
-      title : title,
-      body : body,
-
+   const duplicateNotes = notes.filter(function(note){
+      return note.title === title
    })
 
-   saveNotes(notes)
+   if ( duplicateNotes.length === 0 ){
+
+      notes.push({
+         title : title,
+         body : body,
+   
+      })
+      saveNotes(notes)
+      console.log('New note added !')
+
+   } else { console.log( 'console title taken')
+  
+   }
+  
 }
 
 const saveNotes = function(notes){
